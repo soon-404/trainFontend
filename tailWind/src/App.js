2b8f6d26add258b0ba98741ac2fb3b6b4c140302
swekiiz <best_n.dol@hotmail.com>
2021-08-12 02:12:59 +0700

    insert tailwind class to component

--- tailWind/src/App.js
+++ tailWind/src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center">
+      <header className="bg-dark min-h-screen flex flex-col justify-center items-center text-2xl text-white">
+        <img
+          src={logo}
+          className="h-10-screen w-auto pointer-events-none animate-spin-slow"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- tailWind/tailwind.config.js
+++ tailWind/tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        dark: "#282c34",
+      },
+      height: {
+        "10-screen": "40vh",
+      },
+      textColor: {
+        link: "#61dafb",
+      },
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
+      },
+    },
   },
   variants: {
     extend: {},
