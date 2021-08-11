286c1827dd6f1f19cb24a2101ef3f7ef5e247c98
swekiiz <best_n.dol@hotmail.com>
2021-08-12 02:20:59 +0700

    add style

--- tailWind/src/App.js
+++ tailWind/src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div className="text-center">
-      <header className="bg-dark min-h-screen flex flex-col justify-center items-center text-2xl text-white">
+      <header className="p-8 relative bg-dark min-h-screen flex flex-col justify-center items-center text-2xl text-white">
         <img
           src={logo}
           className="h-10-screen w-auto pointer-events-none animate-spin-slow"
@@ -13,13 +13,16 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="text-link"
+          className="text-link mt-2"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <footer className="text-right text-sm absolute bottom-0 right-0 tracking-tight whitespace-nowrap text-blue-200 p-4 opacity-90">
+          create by <a className="text-yellow-300 underline cursor-pointer font-semibold active:text-gray-100 hover:text-yellow-500" href="https://github.com/swekiiz">swekiiz</a>
+        </footer>
       </header>
     </div>
   );
