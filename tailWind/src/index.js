7e55a28f2ad826cf626ac7991464fe4d96229e1a
soon-404 <tfpsoon@gmail.com>
2021-08-13 22:22:47 +0700

    Navbar + Hero

--- tailWind/src/index.js
+++ tailWind/src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-
+import { BrowserRouter } from "react-router-dom";
 import "./styles/globals.css";
 import "./styles/tailwind.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
